797b1db96b240f2739d822172a6714ea6648c62e
Kay Savage <ksavage@spotify.com>
2019-10-19 17:57:40 -0400

    post_doc

---
+++ post_gen_project.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+print(os.getcwd())  # prints /absolute/path/to/{{cookiecutter.project_slug}}
+
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+create_install_csci_utils = '{{cookiecutter.install_csci_utils}}' == 'yes'
+
+if not create_install_csci_utils:
+    # remove top-level file inside the generated folder
+    remove('{{cookiecutter.project_slug}}')
