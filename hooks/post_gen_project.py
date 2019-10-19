980b765d4adbf63eb63065287b6c39a3c97cc7a8
Kay Savage <ksavage@spotify.com>
2019-10-19 18:46:10 -0400

    make hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,14 @@
 import os
 import shutil
 
- # prints /absolute/path/to/{{cookiecutter.project_slug}}
-print(os.path.join('{{cookiecutter.repo_name}}'))
+print(os.getcwd())  # prints /absolute/path/to/{{cookiecutter.project_slug}}
+print("hello")
+print(os.path.join('{{cookiecutter.repo_name}}', '{{cookiecutter.project_slug}}'))
 
 base_dir = os.getcwd()
+print(os.path.join(base_dir, '{{cookiecutter.repo_name}}','{{cookiecutter.project_slug}}'))
+
+#os.path.join(base_dir, filename)
 
 
 def remove(filepath):
@@ -17,4 +21,7 @@ create_install_csci_utils = '{{cookiecutter.install_csci_utils}}' == 'yes'
 
 if not create_install_csci_utils:
     #remove top-level file inside the generated folder
-    remove(os.path.join(base_dir, '{{cookiecutter.repo_name}}','{{cookiecutter.project_slug}}'))
+    os.remove(os.path.join(base_dir, '{{cookiecutter.repo_name}}','{{cookiecutter.project_slug}}')) #removes a file.
+    os.rmdir(os.path.join(base_dir, '{{cookiecutter.repo_name}}','{{cookiecutter.project_slug}}')) #removes an empty directory.
+    shutil.rmtree(os.path.join(base_dir, '{{cookiecutter.repo_name}}','{{cookiecutter.project_slug}}')) #deletes a directory and all its contents.
+    remove(os.path.join('{{cookiecutter.repo_name}}', '{{cookiecutter.project_slug}}'))
