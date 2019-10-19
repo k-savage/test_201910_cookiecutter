b09ccbc2cf597512f2e86dee9fd747989fc7456b
Kay Savage <ksavage@spotify.com>
2019-10-19 18:07:37 -0400

    try again

--- post_gen_project.py
+++ post_gen_project.py
@@ -11,6 +11,6 @@ def remove(filepath):
 
 create_install_csci_utils = '{{cookiecutter.install_csci_utils}}' == 'yes'
 
-if not create_install_csci_utils:
+#if not create_install_csci_utils:
     # remove top-level file inside the generated folder
-    remove(os.path.join('{{cookiecutter.repo_name}}', '{{cookiecutter.project_slug}}'))
+    #remove(os.path.join('{{cookiecutter.repo_name}}', '{{cookiecutter.project_slug}}'))
