11082b1518eb6afdd6ff26ecd7fa9580d1ed45e9
Kay Savage <ksavage@spotify.com>
2019-10-19 18:25:06 -0400

    initial commit

--- post_gen_project.py
+++ post_gen_project.py
@@ -1,16 +1,16 @@
 import os
 import shutil
 
-#print(os.getcwd())  # prints /absolute/path/to/{{cookiecutter.project_slug}}
+print(os.getcwd())  # prints /absolute/path/to/{{cookiecutter.project_slug}}
 
-#def remove(filepath):
-#    if os.path.isfile(filepath):
-#        os.remove(filepath)
-#    elif os.path.isdir(filepath):
-#        shutil.rmtree(filepath)
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
 
 create_install_csci_utils = '{{cookiecutter.install_csci_utils}}' == 'yes'
 
-#if not create_install_csci_utils:
-    # remove top-level file inside the generated folder
-    #remove(os.path.join('{{cookiecutter.repo_name}}', '{{cookiecutter.project_slug}}'))
+if not create_install_csci_utils:
+    #remove top-level file inside the generated folder
+    remove(os.path.join('{{cookiecutter.repo_name}}', '{{cookiecutter.project_slug}}'))
